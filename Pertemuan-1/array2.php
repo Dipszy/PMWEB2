f74956a836c5c2e0561b167b03f14851d08dc6f9
Dower4you <mhmaddiqbll@gmail.com>
2025-02-26 22:06:28 +0700

    Fix array2.php

--- Pertemuan-1/array2.php
+++ Pertemuan-1/array2.php
@@ -1,45 +1,48 @@
 <?php 
 
-    //Array Sort
-    $ar_buah = ["A" => "Apel", "B" => "Banana", "C" => "Chery" ];
-    echo $ar_buah[2];
+// Array Sort
+$ar_buah = ["A" => "Apel", "B" => "Banana", "C" => "Chery"];
 
-    echo '<br>Jumlah Buah : ' . count($ar_buah);
+// Mengakses indeks yang tidak ada akan menyebabkan error, jadi dihapus
+// echo $ar_buah[2];
 
-    echo '<ol>';
-    foreach($ar_buah as $buah) {
-        echo '<li>' . $buah . '</li>';
-    }
-    echo '</ol>';
-    $ar_buah[] = "Apel";
-    unset($ar_buah[1]);
-    $ar_buah[2] = "Banana";
-    echo '<ul>';
-    foreach($ar_buah as $k => $v} {
-        echo '<li>' buah index - ' . $k . ' adalah ' . $v . ' '</li>';
-    }
+echo '<br>Jumlah Buah : ' . count($ar_buah);
 
+echo '<ol>';
+foreach($ar_buah as $buah) {
+    echo '<li>' . $buah . '</li>';
+}
+echo '</ol>';
 
-    $ar_buah = ["A" => "Apel", "B" => "Banana", "C" => "Chery" ];
-    echo '<ol>';
-    foreach ($ar_buah as $k => $v) {
-        echo '<li>' . $k . '. ' . $v . '</li>';
-    }
-    echo '</ol>'
+$ar_buah[] = "Apel";
+unset($ar_buah[1]); // Tidak ada indeks 1, jadi ini tidak berpengaruh
+$ar_buah["C"] = "Banana"; // Menggunakan key yang benar
 
-    // Array POP
-    $tims = ["erwin", "heru", "ali", "zaki"];
-    array_pop($tims);
-    foreach ($tims as $person) {
-        echo $person . "<br>";
-    }
+echo '<ul>';
+foreach($ar_buah as $k => $v) {
+    echo '<li>Buah index - ' . $k . ' adalah ' . $v . '</li>';
+}
+echo '</ul>';
 
-    // Array PUSH
-    $tims = ["erwin", "heru", "ali", "zaki"];
-    array_push($tims, "wati");
-    foreach ($tims as $person) {
-        echo $person . "<br>";
-    }
+$ar_buah = ["A" => "Apel", "B" => "Banana", "C" => "Chery"];
+echo '<ol>';
+foreach ($ar_buah as $k => $v) {
+    echo '<li>' . $k . '. ' . $v . '</li>';
+}
+echo '</ol>';
 
-    //
+// Array POP
+$tims = ["erwin", "heru", "ali", "zaki"];
+array_pop($tims);
+foreach ($tims as $person) {
+    echo $person . "<br>";
+}
+
+// Array PUSH
+$tims = ["erwin", "heru", "ali", "zaki"];
+array_push($tims, "wati");
+foreach ($tims as $person) {
+    echo $person . "<br>";
+}
+
 ?>
